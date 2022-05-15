2308b4d8407a171b4329cec3881ffb00385ef1ae
minnyres <liuthgw23@gmail.com>
2022-05-15 14:31:05 +0800

    Disable monolithic wx, so amuled will fail to start

--- scripts/wxwidgets.sh
+++ scripts/wxwidgets.sh
@@ -8,7 +8,7 @@ cd wxWidgets-3.0.5
 ./configure CPPFLAGS="-I$BUILDDIR/zlib/include -I$BUILDDIR/libpng/include" \
     LDFLAGS="-L$BUILDDIR/zlib/lib -L$BUILDDIR/libpng/lib" \
     --host=$TARGET --prefix=$BUILDDIR/wxwidgets --with-zlib=sys --with-libpng=sys --with-msw --with-libiconv-prefix=$BUILDDIR/libiconv \
-    --disable-shared --disable-debug_flag --disable-mediactrl --enable-optimise --enable-unicode --enable-monolithic
+    --disable-shared --disable-debug_flag --disable-mediactrl --enable-optimise --enable-unicode
 mkdir -p $BUILDDIR/wxwidgets/lib
 ln -s lib $BUILDDIR/wxwidgets/lib64
 make -j$(nproc)
@@ -24,3 +24,10 @@ if [ "$ARCH" == "win32-arm" ]; then
     rc_line=$(sed -n -e '/error "One of WX_CPU_XXX constants must be defined/=' $WX_INCLUDE/wx.rc)
     sed -i "$rc_line c\wxMANIFEST_ID 24 \"wx/msw/arm.manifest\"" $WX_INCLUDE/wx.rc
 fi
+
+cd $BUILDDIR/wxwidgets/lib
+
+for file in *.a; do
+    newfile="${file//"-$TARGET"/""}"
+    ln -s $file $newfile
+done
